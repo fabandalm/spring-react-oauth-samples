aef62352192e42fc1ea254690596f765dad5a048
Almeida <falmeida.tech@gmail.com>
2020-04-27 19:34:41 -0400

    Initial commit.

---
+++ spring-boot-react-sample/src/main/java/com/falmeida/tech/SpringBootReactSampleApplication.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactSampleApplication.class, args);
+	}
+
+}
